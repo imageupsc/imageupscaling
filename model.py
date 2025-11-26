ae08dc1ecc07b344c3e22fc07a32288c115a82b5
imageupsc <imageupscaling1@kolabnow.com>
2025-11-26 13:44:18 +0400

    Creating simple model

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+
+        self.layer1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        self.layer2 = nn.Conv2d(64, 32, kernel_size=5, padding=2)
+        self.layer3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.layer1(x))
+        x = self.relu(self.layer2(x))
+        x = self.layer3(x)
+        return x
