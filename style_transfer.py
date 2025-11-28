e4325a35153e6e4100be6e20d6181e5041b1a00e
imageupsc <imageupscaling1@kolabnow.com>
2025-11-28 12:37:44 +0400

    Added Transformer class

--- style_transfer.py
+++ style_transfer.py
@@ -33,3 +33,45 @@ class ResidualBlock(nn.Module):
         out = self.in2(self.conv2(out))
         out = out + residual
         return out
+
+
+class TransformerNet(nn.Module):
+    def __init__(self):
+        super(TransformerNet, self).__init__()
+        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
+        self.in1 = nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        self.in3 = nn.InstanceNorm2d(128, affine=True)
+        self.relu = nn.ReLU()
+
+        self.res1 = ResidualBlock(128)
+        self.res2 = ResidualBlock(128)
+        self.res3 = ResidualBlock(128)
+        self.res4 = ResidualBlock(128)
+        self.res5 = ResidualBlock(128)
+
+        self.deconv1 = nn.ConvTranspose2d(
+            128, 64, kernel_size=3, stride=2, padding=1, output_padding=1
+        )
+        self.in4 = nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = nn.ConvTranspose2d(
+            64, 32, kernel_size=3, stride=2, padding=1, output_padding=1
+        )
+        self.in5 = nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
+
+    def forward(self, x):
+        y = self.relu(self.in1(self.conv1(x)))
+        y = self.relu(self.in2(self.conv2(y)))
+        y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        y = self.res4(y)
+        y = self.res5(y)
+        y = self.relu(self.in4(self.deconv1(y)))
+        y = self.relu(self.in5(self.deconv2(y)))
+        y = self.deconv3(y)
+        return y
